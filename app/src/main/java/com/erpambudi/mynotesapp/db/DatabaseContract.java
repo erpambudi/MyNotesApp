ceb6508946d6c572878d93db90569893f9bfc0bb
erpambudi <erpambudi76@gmail.com>
2019-12-25 17:52:04 +0700

    add provider

--- app/src/main/java/com/erpambudi/mynotesapp/db/DatabaseContract.java
+++ app/src/main/java/com/erpambudi/mynotesapp/db/DatabaseContract.java
@@ -1,13 +1,24 @@
 package com.erpambudi.mynotesapp.db;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class DatabaseContract {
     public static String TABLE_NAME = "note";
+    public static final String AUTHORITY = "com.erpambudi.mynotesapp";
+    private static final String SCHEME = "content";
+
+    private DatabaseContract(){}
 
     public static final class NoteColumns implements BaseColumns {
         public static String TITLE = "title";
         public static String DESCRIPTION = "description";
         public static String DATE = "date";
+
+        // untuk membuat URI content://com.dicoding.picodiploma.mynotesapp/note
+        public static final Uri CONTENT_URI = new Uri.Builder().scheme(SCHEME)
+                .authority(AUTHORITY)
+                .appendPath(TABLE_NAME)
+                .build();
     }
 }
